0d1d439c5525cac52ec8a4e79a8bf89a88c89e8c
Nathan Hupka <nathanhupka@uofs-10-227-138-90.usask.ca>
2017-09-20 12:13:39 -0600

    Player can dettach from the pivot point and reattach.

--- Assets/Swing.cs
+++ Assets/Swing.cs
@@ -12,14 +12,29 @@ public class Swing : MonoBehaviour {
 		public double x;
 		public double y;
 	}
+	enum PendulumStates { Free, Swinging };
+	PendulumStates PendulumState;
 	float accel;
 	VectorD2 pivot;
+	GameObject circle;
 	Rigidbody2D pendulum;
 	LineRenderer[] lines;
 	double radius;
 	double radiusDelta;
 	// Use this for initialization
 	void Start () {
+		PendulumState = PendulumStates.Swinging;
+		circle = GameObject.Find("Dot");
+		for (int j = 1; j <= 3 ; j++) {
+			double angle = Math.PI * 2;
+			for (int i = 0; i < 5; i++) {
+				GameObject _circle = Instantiate (circle);
+				double x = Math.Cos (angle) * ((5 * Convert.ToInt64(j > 1) * (j-1)) + Convert.ToInt64(j == 1));
+				double y = Math.Sin (angle) * ((5 * Convert.ToInt64(j > 1) * (j-1)) + Convert.ToInt64(j == 1));	
+				_circle.transform.position = new Vector2 ((float)x, (float)y);
+				angle -= Math.PI / 4;
+			}
+		}
 		radius = 5;
 		radiusDelta = 0;
 		pivot = new VectorD2(0,0);
@@ -31,6 +46,17 @@ public class Swing : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			switch (PendulumState) {
+			case PendulumStates.Free:
+				PendulumState = PendulumStates.Swinging;
+				radius = Math.Sqrt ((Math.Pow (pendulum.position.x, 2) + Math.Pow (pendulum.position.y, 2)));
+				break;
+			case PendulumStates.Swinging:
+				PendulumState = PendulumStates.Free;
+				break;
+			}
+		}
 		if (Input.GetKeyDown (KeyCode.UpArrow))
 			radiusDelta += -0.1;
 		else if (Input.GetKeyUp (KeyCode.UpArrow))
@@ -41,7 +67,7 @@ public class Swing : MonoBehaviour {
 			radiusDelta -= 0.1;
 		
 		if (radiusDelta < 0) {
-			if ((radius > Math.Abs (radiusDelta)) && (radius > 0.5)) {
+			if ((radius > Math.Abs (radiusDelta)) && (radius + radiusDelta > 1)) {
 				radius += radiusDelta;
 			}
 		} else {
@@ -64,11 +90,29 @@ public class Swing : MonoBehaviour {
 		} else if (y < 0) {
 			angle += 2 * Math.PI;
 		}
+		double layer;
+		double hyp = Math.Sqrt ((Math.Pow (x, 2) + Math.Pow (y, 2)));
+		if (hyp < radius) {
+			layer = 1;
+		} else {
+			layer = -1;
+		}
 		lines[0].SetPosition (1, new Vector3 (pendulum.position.x, pendulum.position.y, 0));
-		lines[1].SetPosition (1, new Vector3 ((float)(Math.Cos(angle) * radius), (float)(Math.Sin(angle) * radius), -1));
+		lines[1].SetPosition (1, new Vector3 ((float)(Math.Cos(angle) * radius), (float)(Math.Sin(angle) * radius), (float)layer));
 	}
 
-	void FixedUpdate () {
+	void FixedUpdate (){
+		switch (PendulumState) {
+		case PendulumStates.Free:
+				
+			break;
+		case PendulumStates.Swinging:
+			SwingingUpdate ();
+			break;
+		}
+	}
+
+	void SwingingUpdate () {
 		VectorD2 tension = new VectorD2(0,0);
 		double centrifugal = 0.00000;
 		double x;
@@ -88,11 +132,11 @@ public class Swing : MonoBehaviour {
 			if (angle > Math.PI) centrifugal += normalForce * Math.Sin (angle);
 			double tangentialV = -1 * (pendulum.velocity.x * Math.Sin (angle)) + (pendulum.velocity.y * Math.Cos (angle));
 			double perpindicularV =  (pendulum.velocity.x * Math.Cos (angle)) + (pendulum.velocity.y * Math.Sin (angle));
-			double angularV = tangentialV / ((radius)/2);
-			//double angularV = tangentialV / ((radius * 1.1 + hyp * .9)/2);
+			//double angularV = tangentialV / ((radius)/2);
+			double angularV = tangentialV / ((radius + hyp)/2);
 			centrifugal += perpindicularV * 30;
-			//centrifugal += pendulum.mass * Math.Pow (angularV, 2) * ((radius * 1.1 + hyp * .9)/2);
-			centrifugal += pendulum.mass * Math.Pow (angularV, 2) * ((radius)/2);
+			centrifugal += pendulum.mass * Math.Pow (angularV, 2) * ((radius + hyp)/2);
+			//centrifugal += pendulum.mass * Math.Pow (angularV, 2) * ((radius)/2);
 			if (radiusDelta < 0)
 				centrifugal += (hyp - radius) * 100;//(hyp - radius) * normalForce * Math.Sin (angle) ;
 		}
