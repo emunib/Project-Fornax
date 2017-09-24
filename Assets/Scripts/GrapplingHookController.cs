74250c0e9327283fbc531e676b4407a0e13270d2
Nathan Hupka <nathanhupka@Nathans-MacBook-Pro.local>
2017-09-23 22:13:19 -0600

    Implemented a system to simulate the rope wrapping around objects. Still needs some work and fails at high speeds.

--- Assets/Scripts/GrapplingHookController.cs
+++ Assets/Scripts/GrapplingHookController.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 public class GrapplingHookController : C_WorldObjectController {
 	LineRenderer RopeLine;
 	Rigidbody2D Body;
-	Stack<Pivot> Pivots;
-
+	public List<Pivot> Pivots;
+	bool Active;
 	// Use this for initialization
 	void Start () {
 		RopeLine = gameObject.GetComponent<LineRenderer>();
 		Body = gameObject.GetComponent<Rigidbody2D> () ;
 		Manager.ObjectLog.Add (gameObject, this);
-		Pivots = new Stack<Pivot>();
-
+		Pivots = new List<Pivot>();
+		Pivots.Add (new Pivot (Body.position));
+		Active = true;
 	}
 
 	void OnDestroy () {
@@ -23,8 +24,15 @@ public class GrapplingHookController : C_WorldObjectController {
 	// Update is called once per frame
 	void Update () {
 		GrapplingHook hook = Object as GrapplingHook;
-		RopeLine.SetPosition (0, new Vector3 (Body.position.x, Body.position.y));
-		RopeLine.SetPosition (1, new Vector3 (hook.PlayerBody.position.x, hook.PlayerBody.position.y));
+		if (Active) {
+			Pivots [0].Position = Body.position;
+		}
+		RopeLine.positionCount = Pivots.Count + 1;
+		RopeLine.SetPosition (0, new Vector3(hook.PlayerBody.position.x, hook.PlayerBody.position.y));
+		int i = Pivots.Count;
+		foreach (Pivot pivot in Pivots){
+			RopeLine.SetPosition (i--, new Vector3(pivot.Position.x, pivot.Position.y));
+		}
 		Vector3 screenPos = Camera.main.WorldToScreenPoint(new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y, 0));
 		if ((screenPos.x < Camera.main.pixelRect.xMin) || (screenPos.x > Camera.main.pixelRect.xMax) || (screenPos.y < Camera.main.pixelRect.yMin) || (screenPos.y > Camera.main.pixelRect.yMax)) {
 			GameObject.Destroy (this.gameObject);
@@ -35,8 +43,12 @@ public class GrapplingHookController : C_WorldObjectController {
 	void OnCollisionEnter2D(Collision2D collision){
 		if ( Manager.ObjectLog[collision.gameObject].Object.GetType() == typeof(Tile)) {
 			GrapplingHook hook = Object as GrapplingHook;
-			hook.PlayerObject.CreateAnchor (Body.position.x, Body.position.y);
-            Body.bodyType = RigidbodyType2D.Static;
+			Vector2 direction = collision.contacts[0].point -  Body.position;
+			direction = -direction/direction.magnitude;
+			Pivots [0].Position = collision.contacts[0].point + (direction * 0.1f);
+			hook.PlayerObject.CreateAnchor (Pivots [0].Position.x, Pivots [0].Position.y, RopeLine);
+			Body.simulated = false;
+			Active = false;
             //GameObject.Destroy (this.gameObject);
 		}
 	}
