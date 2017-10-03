b5d11b86a8dcd5e2312f6392c9bb3f51badfd258
Fung <waf045@usask.ca>
2017-10-03 15:26:41 -0600

    Wrote some concepts/ideas for the procedural level generation

--- Assets/Scripts/LevelBuilder.cs
+++ Assets/Scripts/LevelBuilder.cs
@@ -12,19 +12,19 @@ public class LevelBuilder : MonoBehaviour
 	public Transform Hazard;
 
 	int[,] map = new int[height, width];
-//	int[,] map = 
-//	{
-//		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1},
-//		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 3, 0, 0, 0, 1, 0, 0, 0, 0},
-//		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
-//		{0, 0, 0, 3, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
-//		{0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
-//		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0},
-//		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
-//		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
-//		{1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1}
-//	};
-
+	/*int[,] map = 
+	{
+		{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 3, 0, 0, 0, 1, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+		{0, 0, 0, 3, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+		{0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+		{1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1}
+	};
+    */
 	// Use this for initialization
 	void Start()
 	{
@@ -50,6 +50,26 @@ public class LevelBuilder : MonoBehaviour
 		}
 	}
 	
+/*
+ * Possible Level Construction Stages:
+ * 1) Decide if horizontally/vertically symmetrical or assymetrical.  Construct with half array for symmetrical levels.
+ * 2) Randomize optional choice of solid walls, ceiling, and floor.
+ * 3) Randomize spawn points, only 2 if symmetrical level, constrained by minimum distance between each other and mirrored side.
+ * 4) Place (generic) premade platforms or generated platforms (platform generator) based on the following possible constraints:
+ *      - There must be at least one platform located below the spawn points or within reach of grappling hook/initial acceleration movement. (spawns can be in the air)
+ *      - If spawns are vertically aligned (within +/- x tiles), there must be a platform between them.
+ *      - Build from bottom up, when placing platforms, randomize platform distances based on a density constraint to achieve:
+ *          - Some platforms directly above each other have vertical distances which are equal or less than grappling hook rope max length. (smooth swinging)
+ *          - Other platforms areas would be more suitable for jumping between and above.
+ *      - Example density: Tile/platform density is higher at the bottom than at the top.
+ *      - Delete tiles/platforms if necessary.
+ * 5) Trap placement algorithm. (could be during platform creation or use of AI and A* pathfinding to determine most/least travelled tiles on the map)
+ * 6) For symmetrical levels, mirror array depending on vertical/horizontal symmetry.
+ * 7) Randomly choose a theme and construct level using appropriate themed tiles and background.
+*/
+
+
+
 	void BuildMap()
 	{
 		for (int i = 0; i < 13; i++)
