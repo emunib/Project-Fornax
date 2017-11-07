881c010c5fbdbea0124f21b8f8f60f9dce7ef588
Nathan Hupka <nathanhupka@uofs-10-227-138-90.usask.ca>
2017-11-07 15:39:10 -0600

    Commented the pendulum controller code. Changed the code for picking the secondary anchor when switching the pendulum and the anchor

--- Assets/Scripts/C_PendulumController.cs
+++ Assets/Scripts/C_PendulumController.cs
@@ -38,6 +38,8 @@ public class C_PendulumController {
 	public float Radius;
 
 	public C_PendulumController(C_PlayerController player, GrapplingHookController hook){
+		// The 'pendulum' is the grappling hook rigidbody to start.
+		// The 'anchor' or the first pivot is the player rigidbody to start.
 		Hook = hook;
 		Pivots = hook.Pivots;
 		Pendulum = hook.Body;
@@ -49,6 +51,9 @@ public class C_PendulumController {
 	}
 
 	public void Switch(){
+		//Switch the pendulum and the anchor
+		//Remove the first pivot, which is the player rigidbody
+		//Reverse the order of the remaining pivots
 		Pivots.RemoveAt (0);
 		List < Pivot> nList = new List<Pivot> ();
 		while (Pivots.Count > 0) {
@@ -65,11 +70,21 @@ public class C_PendulumController {
 		Pendulum = Player.body;
 		Anchor = Hook.Body;
 		Vector2 secondaryAnchor;
+
+		//Set the position of the new pivot to the position of the new anchor
+		//Adjust the position of the new pivot until it is not inside any colliders
 		if (Pivots.Count > 1) {
 			secondaryAnchor = Pivots [1].Position;
-			if ((secondaryAnchor - pivot.Position).magnitude < 0.75) {
+
+			while (((secondaryAnchor - pivot.Position).magnitude < 0.75) && (Pivots.Count > 1)) {
+				//If the secondary anchor is too close to the new pivot, the raycast will not detect colliders properly
+				//Too avoid this delete the offending pivot and set the secondary anchor to the next pivot inline
 				Pivots.Remove (Pivots [1]);
-				secondaryAnchor = Pendulum.position;
+				if (Pivots.Count > 1) {
+					secondaryAnchor = Pivots [1];
+				} else {
+					secondaryAnchor = Pendulum.position;
+				}
 			}
 		} else {
 			secondaryAnchor = Pendulum.position;
@@ -77,6 +92,7 @@ public class C_PendulumController {
 
 		RaycastHit2D empty = new RaycastHit2D ();
 		RaycastHit2D result = new RaycastHit2D ();
+
 		Loop.WhileOrFor (() => ((result = GetHits (pivot.Position, secondaryAnchor, empty)) != empty),
 			() => {
 				Vector2 directionVec = (secondaryAnchor - new Vector2 (result.collider.bounds.center.x, result.collider.bounds.center.y));
@@ -88,7 +104,9 @@ public class C_PendulumController {
 				}
 			}, 100);
 				
+		//Move the grappling hook to the modified position
 		Hook.transform.position = pivot.Position;
+		//Set the radius to the distance between the nearest pivot and the pendulum
 		Radius = (Pivots[Pivots.Count -1].Position - Pendulum.position).magnitude;
 	}
 
@@ -124,9 +142,12 @@ public class C_PendulumController {
 	}
 
 	public void Draw(){
+		//If the anchor is an active physics object then update the position of the fatherest pivot, which should be the anchor
 		if (Anchor.simulated == true) {
 			Pivots [0].Position = Anchor.position;
 		}
+
+		//Draw a line with all the pivots as points in the line
 		Hook.RopeLine.positionCount = Pivots.Count + 1;
 		Hook.RopeLine.SetPosition (0, new Vector3(Pendulum.position.x, Pendulum.position.y));
 		int i = Pivots.Count;
@@ -142,6 +163,9 @@ public class C_PendulumController {
 		RaycastHit2D[] collidees = Physics2D.RaycastAll (pivot.Position, line2pend);
 		RaycastHit2D collidee = new RaycastHit2D();
 		bool notfound = true;
+
+		//Draw a line from the nearest pivot to the pendulum
+		//and get the closest hit to the pivot that is not the anchor itself
 		foreach (RaycastHit2D hit in collidees){
 			if (Manager.ObjectLog [hit.collider.gameObject] != AnchorObject) {
 				if (notfound) {
@@ -153,25 +177,41 @@ public class C_PendulumController {
 			}
 		}
 
+
+		//The nearest hit should be the pendulum. If it is not then a new pivot needs to be added
 		if (Manager.ObjectLog [collidee.collider.gameObject] != PendulumObject) {	
+			// If the nearest pivot is inside of the object that the raycast hit then the logic 
+			// of the algorithm has failed, throw an error
 			if (collidee.collider.bounds.Contains (Pivots[Pivots.Count - 1].Position)) {
 				throw new Exception ("pivot inside of bounding box");
 			}
+
+			// This directionVec should lead away from object that the raycast hit
 			Vector2 directionVec = (collidee.point - new Vector2 (collidee.collider.bounds.center.x, collidee.collider.bounds.center.y));
 			directionVec.Normalize ();
+
+			// Move the point of the hit away from the collider
 			Vector2 collisionPoint = collidee.point + (0.1f * directionVec);
+			// While the point is inside of the collider move it away from the center
 			while (collidee.collider.bounds.Contains (collisionPoint)) {
 				 collisionPoint += 0.1f * directionVec;
 			}
 
 			Vector2 line2collision =  collisionPoint - pivot.Position;
 			Pivots.Add (new Pivot (collisionPoint, pivot));
+
+			// If the new pivot is inside of the object that the raycast hit then the logic 
+			// of the algorithm has failed, throw an error
 			if (collidee.collider.bounds.Contains (Pivots[Pivots.Count - 1].Position)) {
 				throw new Exception ("pivot inside of bounding box");
 			}
+
+			// Adjust the radius to account for the length that has been consumed by the new pivot
 			Radius -= line2collision.magnitude;
 		} else {
 			if (Pivots.Count > 1) {
+				// If the pendulum can 'see' the pivot behind the nearest pivot then the nearest pivot is unecessary and
+				// can be removed
 				RaycastHit2D[] hits = Physics2D.LinecastAll (Pendulum.position, Pivots [Pivots.Count - 2].Position);
 				if (hits.Length == 1) {
 					Radius += (Pivots [Pivots.Count - 2].Position - (Pivots [Pivots.Count - 1].Position)).magnitude;
@@ -179,36 +219,53 @@ public class C_PendulumController {
 				}
 			}
 		}
+
+		// Grab the nearest pivot after adding or deleting any pivots
 		pivot = Pivots [Pivots.Count - 1];
 		VectorD2 tension = new VectorD2(0,0);
 		double centrifugal = 0.00000;
 		double hyp;
 		double normalForce = Physics2D.gravity.y * Pendulum.mass;
+
 		line2pend = Pendulum.position - pivot.Position;
 		hyp = line2pend.magnitude;
 		angle = Trig.GetAngle (line2pend);
+
+		//If the rope is 'taut' then the forces of the rope apply
 		if ((hyp >= Radius)) {
+			// Calculate tension due to the normal force
 			if (angle > Math.PI) centrifugal += normalForce * Math.Sin (angle);
 			double tangentialV = -1 * (Pendulum.velocity.x * Math.Sin (angle)) + (Pendulum.velocity.y * Math.Cos (angle));
 			double perpindicularV =  (Pendulum.velocity.x * Math.Cos (angle)) + (Pendulum.velocity.y * Math.Sin (angle));
 			//double angularV = tangentialV / ((radius)/2);
 			double angularV = tangentialV / ((Radius + hyp)/2);
 			centrifugal += perpindicularV * 30;
+			// Calculate tension from centrifugal force
 			centrifugal += Pendulum.mass * Math.Pow (angularV, 2) * ((Radius + hyp)/2);
 			//centrifugal += pendulum.mass * Math.Pow (angularV, 2) * ((radius)/2);
+
+			//Apply a corrective force to the tension
+			//This force should keep the radius stable and prevent small variations in calculation
+			// and errors in the physics system due to the granularity of simulated time from accumulating;
 			centrifugal += (hyp - Radius) * 50;//(hyp - radius) * normalForce * Math.Sin (angle) ;
 		}
 		tension.x = 0;
 		tension.y = 0;
 		Vector2 perpenline = new Vector2 (Mathf.Abs(line2pend.x), -Mathf.Sign(line2pend.x)*(line2pend.y));
 		perpenline.Normalize ();
+
+		// Apply the force of the player swinging the rope
 		if (angle > Math.PI) {
 			tension.y += Player.AngularAccel * perpenline.y * 10;//Math.Cos ((Math.PI * 2) - angle);
 			tension.x += Player.AngularAccel * perpenline.x * 10; //Math.Sin ((Math.PI * 2) - angle);
 		}
 
+
+		// Apply the tension of the rope
 		tension.y += centrifugal * -Math.Sin (angle);
 		tension.x += centrifugal * -Math.Cos (angle);
+
+		// Apply the force to the pendulum
 		Pendulum.AddForce (new Vector2((float)tension.x, (float)tension.y), ForceMode2D.Force);
 	}
 
