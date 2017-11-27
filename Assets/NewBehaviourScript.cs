9d0af5e7af0557e657de0206a75ef3f5920ab284
Unknown <ehm011@mail.usask.ca>
2017-11-27 16:36:49 -0600

    Add background assets, and som GUI

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+	private Button[] buttons;
+	// Use this for initialization
+	void Start () {
+		buttons = this.GetComponentsInChildren<Button>(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
